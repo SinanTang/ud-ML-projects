ecdf042d74dd2798f82c0c6eef1c342c119436a7
Sinan Tang <tangsinan92@gmail.com>
2018-01-02 17:25:40 +0100

    SVM miniproject finished

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,7 +16,7 @@ from time import time
 from ..tools.email_preprocess import preprocess
 ## to deal with (deprecated) relative imports in Python 3.x,
 ## run this shell line outside the ud120-projects dir:
-# $ python3 -m ud120-projects.naive_bayes.nb_author_id
+# $ python3 -m ud-ML-projects.naive_bayes.nb_author_id
 
 ### features_train and features_test are the features for
 ### the training and testing datasets, respectively
@@ -27,21 +27,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-def NBAcuracy(features_train, labels_train, features_test, labels_test):
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
     from sklearn.naive_bayes import GaussianNB
     clf = GaussianNB()
+
     t0 = time()
     clf.fit(features_train, labels_train)
     print("training time:", round(time() - t0), "s")
+    # 2s
 
     t1 = time()
     pred = clf.predict(features_test)
     print("prediction time:", round(time() - t1), "s")
+    # 0s
 
     from sklearn.metrics import accuracy_score
     accuracy = accuracy_score(labels_test, pred)
     return accuracy
+    # 0.97
 
-print("Accuracy Score:", NBAcuracy(features_train, labels_train, features_test, labels_test))
+print("Accuracy Score:", NBAccuracy(features_train, labels_train, features_test, labels_test))
 
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+# sys.path.append("../tools/")
+from ..tools.email_preprocess import preprocess
+# $ python3 -m ud-ML-projects.svm.svm_author_id
 
 
 ### features_train and features_test are the features for the training
@@ -20,11 +21,39 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+def SVMaccuracy(features_train, labels_train, features_test, labels_test):
+    from sklearn.svm import SVC
+    # clf = SVC(kernel="linear")
+    clf = SVC(C=10000., kernel="rbf") # 1k+ s when trained on full dataset..
+    # clf = SVC()
+
+    # # cutting down on training dateset
+    # features_train = features_train[:len(features_train)//100]
+    # labels_train = labels_train[:len(labels_train)//100]
+
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print("training time:", round(time() - t0), "s")
+    # linear: 161s / 0s
+
+    t1 = time()
+    pred = clf.predict(features_test)
+    print("prediction time:", round(time() - t1), "s")
+    # linear: 16s / 1s
+
+    # print(pred[10], pred[26], pred[50])
+
+    # how many Chris emails (lable 1) detected?
+    print("Sum:", sum(pred))
+
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    return acc
+    # linear: 0.98 / 0.88
+    # rbf + full data: 0.99
+
+print("SVM accuracy score:", SVMaccuracy(features_train, labels_train, features_test, labels_test))
 
 #########################################################
-
-
